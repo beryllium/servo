fa4bef9f29a00326dda465fa1111340f002c9e6d
Kevin Boyd <kevin.boyd@gmail.com>
2026-07-31 16:58:30 -0700

    Add initial macports support - has some peculiar behaviours

--- python/servo/platform/macos.py
+++ python/servo/platform/macos.py
@@ -41,18 +41,35 @@ class MacOS(Base):
         return not target.is_cross_build() and os.path.exists(GSTREAMER_ROOT)
 
     def _platform_bootstrap(self, force: bool, yes: bool) -> bool:
-        installed_something = False
-        try:
-            brewfile = os.path.join(os.path.dirname(__file__), "Brewfile")
-            output = subprocess.check_output(["brew", "bundle", "install", "--file", brewfile]).decode("utf-8")
-            print(output)
-            installed_something = "Installing" in output
-        except subprocess.CalledProcessError as e:
-            print("Could not run homebrew. Is it installed?")
-            raise e
+        installed_something = (self._platform_bootstrap_brew or self._platform_bootstrap_macports)
+
+        if installed_something:
+            print("Could not install dependencies. Make sure you have Homebrew or MacPorts installed.")
+            return False
+
         target = BuildTarget.from_triple(None)
         installed_something |= self._platform_bootstrap_gstreamer(target, force, yes)
         return installed_something
+
+    def _platform_bootstrap_brew(self) -> bool:
+        brewfile = os.path.join(os.path.dirname(__file__), "Brewfile")
+        try:
+            output = subprocess.check_output(["brew", "bundle", "install", "--file", brewfile]).decode("utf-8")
+        except subprocess.CalledProcessError:
+            return False
+
+        print(output)
+        return "Installing" in output
+
+    def _platform_bootstrap_macports(self) -> bool:
+        print("Trying MacPorts...")
+        macportsScript = os.path.join(os.path.dirname(__file__), "macports.sh")
+        try:
+            result = subprocess.run([macportsScript], check=True)
+        except subprocess.CalledProcessError:
+            return False
+
+        return result.returncode == 0
 
     def _platform_bootstrap_gstreamer(self, target: BuildTarget, force: bool, yes: bool) -> bool:
         if not force and self.is_gstreamer_installed(target):
---
+++ python/servo/platform/macports.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Please keep this in sync with the Brewfile
+port install cmake
+port install pkgconfig
